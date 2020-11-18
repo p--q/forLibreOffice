8a4d4028428f79fb0762eef22179fc85a11b70e8
pq <pq@VJ24LXUVHT8B>
2020-11-18 12:41:09 +0900

    HP

--- IDLextraction/src/createHTML.py
+++ IDLextraction/src/createHTML.py
@@ -2,26 +2,39 @@
 # -*- coding: utf-8 -*-
 import subprocess 
 import uno
+from com.sun.star.uno.TypeClass import ENUM, STRUCT, CONSTANTS  # enum
 sdk_path = "/opt/libreoffice6.4/sdk/idl"  # idlフォルダへのパス。
 ctx = uno.getComponentContext()
 tdm = ctx.getByName('/singletons/com.sun.star.reflection.theTypeDescriptionManager')
-idls = lambda q: subprocess.run(fr"grep -rl '{q}' {sdk_path}|sed -E 's@{sdk_path}/(\S+?)\.idl$@\1@;s@/@.@g'|sort", shell=True, capture_output=True).stdout.decode().split("\n")[:-1]  # シングルクォーテーションだと$が使えない。/をエスケープしないために@を使う。パイプを使うためにshell=Trueにする。
+idlset = lambda q: set(subprocess.run(fr"grep -rl '{q}' {sdk_path}|sed -E 's@{sdk_path}/(\S+?)\.idl$@\1@;s@/@.@g'", shell=True, capture_output=True).stdout.decode().splitlines())  # シングルクォーテーションだと$が使えない。/をエスケープしないために@を使う。パイプを使うためにshell=Trueにする。
 base_url = lambda t, i: fr"https://api.libreoffice.org/docs/idl/ref/{t}{'_1_1'.join(i.split('.'))}.html"  # t:type, i:IDL
+atag = lambda t, i, m: fr'<a target="_blank" href="{base_url(t, i)}">{i}</a> {m}<br>'
+deprecated_set = idlset("@deprecated ")
 # struct
-structs = [fr'<a href="{base_url("struct", i)}">{i}</a> {" ".join(tdm.getByHierarchicalName(i).getMemberNames())}<br>' for i in idls('published struct')]
+structs = []
+structs_list = sorted(idlset('struct ').difference(deprecated_set))
+for i in structs_list:
+	td = tdm.getByHierarchicalName(i)
+	if td.getTypeClass()==STRUCT:
+		structs.append(atag("struct", i, td.getMemberNames()))
 with open("output_struct.html", "w") as f:
 	f.write("\n".join(structs))
 # constants
-constants = [fr'<a href="{base_url("namespace", i)}">{i}</a> {" ".join(j.getName().rsplit(".", 1)[1] for j in tdm.getByHierarchicalName(i).getConstants())}<br>' for i in idls('published constants')]
+constants = []
+constants_list = sorted(idlset('constants ').difference(deprecated_set))
+for i in constants_list:
+	td = tdm.getByHierarchicalName(i)
+	if td.getTypeClass()==CONSTANTS:
+		constants.append(atag("namespace", i, [j.getName().rsplit(".", 1)[1] for j in td.getConstants()]))
 with open("output_constants.html", "w") as f:
 	f.write("\n".join(constants))
-# enum
-# enums = [fr'<a href="{base_url("namespace", i)}">{i}</a> {" ".join(tdm.getByHierarchicalName(i).getEnumNames())}<br>' for i in idls('enum ')]
-# with open("output_enum.html", "w") as f:
-# 	f.write("\n".join(enums))
-# for i in idls('enum '):
-# 	print(i)
-# 	print(tdm.getByHierarchicalName(i).getEnumNames())
-	
-# 	@deprecated 削る
-	
+# enums
+enums = []
+enums_list = sorted(idlset('enum ').difference(deprecated_set))
+base_url = lambda t, i: fr"https://api.libreoffice.org/docs/idl/ref/{t}{'_1_1'.join(i.split('.')[:-1])}.html#details"  # t:type, i:IDL
+for i in enums_list:
+	td = tdm.getByHierarchicalName(i)
+	if td.getTypeClass()==ENUM:
+		enums.append(atag("namespace", i, td.getEnumNames()))
+with open("output_enum.html", "w") as f:
+	f.write("\n".join(enums))
